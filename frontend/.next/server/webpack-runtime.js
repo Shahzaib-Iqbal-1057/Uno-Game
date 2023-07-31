0f276753731a14b5ea1fcb10e0b68f66b47f09f8
Shahzaib-Iqbal-1057 <shahzaibiqbal1057@gmail.com>
2023-07-31 14:16:32 +0500

    converted frontend to next js and made the backend compatible for 2 and 3 players as well10

--- frontend/.next/server/webpack-runtime.js
+++ frontend/.next/server/webpack-runtime.js
@@ -125,7 +125,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("573fd93677c0d4d4")
+/******/ 		__webpack_require__.h = () => ("1435bba0efca9d11")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
--- frontend/.next/static/chunks/webpack.js
+++ frontend/.next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "df0fc842315f3d68"; }
+/******/ 		__webpack_require__.h = function() { return "af0d2089c65ed8dc"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
