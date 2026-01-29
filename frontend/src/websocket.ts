583389cb2f488bbdfdfb0efcb634a4dacad42e04
Shahzaib Iqbal <98122801+Shahzaib-Iqbal-1057@users.noreply.github.com>
2026-01-29 21:36:34 +0500

    added a print statement

--- frontend/src/websocket.ts
+++ frontend/src/websocket.ts
@@ -3,11 +3,11 @@ import { io, Socket } from "socket.io-client";
 const SERVER_URL =
   import.meta.env.VITE_BACKEND_URL?.trim() || "http://localhost:3001";
 
-console.log("server url : ", SERVER_URL)
 
 let socket: Socket | null = null;
 
 export const initializeSocket = (): Socket => {
+  console.log("server url : ", SERVER_URL)
   if (!socket) {
     socket = io(SERVER_URL, {
       transports: ["websocket"], // avoid polling issues in prod
